fb054c27b6979b8db46fcf339506ad54cdfed02f
benbloomie <bloomfib@mcmaster.ca>
2025-01-22 14:18:15 -0500

    adding initial logic for analyzing the direction of the explorer.

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Direction.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Direction.java
@@ -1,5 +1,38 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 public class Direction {
+    private int currentDirection; 
+
+    public Direction(String startingDirection) {
+        determineDirection(startingDirection);
+    }
+
+    public void determineDirection(String direction) {
+        // uses a string to represent the direction that the explorer is currently facing
+        switch(direction) {
+            case "N":
+                this.currentDirection = 0;
+            case "E":
+                this.currentDirection = 1;
+            case "S":
+                this.currentDirection = 2;
+            case "W":
+                this.currentDirection = 3;
+        }
+    }
+
+    public void moveExplorer(String move) {
+        // uses modular arithmetic to calculate the new position of the explorer
+        if (move.equals("L")) {
+            this.currentDirection = (this.currentDirection + 3) % 4;
+        }
+        if (move.equals("R")) {
+            this.currentDirection = (this.currentDirection + 1) % 4;
+        }
+    }
+
+    public int getFacingDirection() {   
+        return this.currentDirection;
+    }
     
 }
--- src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeExplorer.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeExplorer.java
@@ -1,5 +1,10 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 public class MazeExplorer {
+    private String moveSequence;
+
+    public MazeExplorer(String moveSequence) {
+        this.moveSequence = moveSequence;
+    }
 
 }
