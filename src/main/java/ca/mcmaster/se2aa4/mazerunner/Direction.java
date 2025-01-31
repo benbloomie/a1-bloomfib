894eff264f2feb48e9a1391e59e755b8d5af6b97
benbloomie <bloomfib@mcmaster.ca>
2025-01-31 11:59:46 -0500

    refactor: removing switch cases from DirectionAnalyzer to use the Direction enum for the movement logic - Resolves #17

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Direction.java
@@ -0,0 +1,97 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+
+// we can use an enum for the direction, as they are a predefined set of values that don't change 
+public enum Direction {
+    NORTH(0) {
+        @Override
+        public void makeMove(int[] position, Maze maze) {
+            // assigns current row and column to variables for better readability 
+            int currentRow = position[0];
+            int currentColumn = position[1];
+            // moves the explorer up when facing north
+            if (currentRow - 1 >= 0 && maze.getTile(currentRow - 1, currentColumn) == ' ') {
+                position[0] = currentRow - 1;
+            }
+        }
+    },
+
+    EAST(1) {
+        @Override
+        public void makeMove(int[] position, Maze maze) {
+            int currentRow = position[0];
+            int currentColumn = position[1];
+            // moves the explorer to the right when facing east
+            if (currentColumn + 1 < maze.getLength() && maze.getTile(currentRow, currentColumn + 1) == ' ') {
+                position[1] = currentColumn + 1;
+            }
+        }
+    },
+
+    SOUTH(2) {
+        @Override
+        public void makeMove(int[] position, Maze maze) {
+            int currentRow = position[0];
+            int currentColumn = position[1];
+            // moves the explorer down when facing south
+            if (currentRow + 1 < maze.getHeight() && maze.getTile(currentRow +1, currentColumn) == ' ') {
+                position[0] = currentRow + 1;
+            }
+        }
+    },
+
+    WEST(3) {
+        @Override
+        public void makeMove(int[] position, Maze maze) {
+            int currentRow = position[0];
+            int currentColumn = position[1];
+            // moves the explorer to the left when facing west
+            if (currentColumn - 1 >= 0 && maze.getTile(currentRow, currentColumn - 1) == ' ') {
+                position[1] = currentColumn - 1;
+            }
+        }
+    };
+
+    private int directionValue;  
+    Direction(int directionValue) {
+        this.directionValue = directionValue;  // assigns a field to each enum --> int representation of the direction
+    }
+
+    // getter method to retrieve the direction the explorer faces as an int
+    public int getDirectionValue() {
+        return this.directionValue;
+    }
+
+    // getter method to retreive the array of directions
+    public Direction[] getDirections() {
+        return values();
+    }
+
+    public Direction setInitialDirection(char startingDirection) {
+        Direction[] directionArray = getDirections();
+        // loops over each direction in the enum
+        for (Direction direction: directionArray) {
+            // if the first character of the direction matches the starting direction, intialize that as the starting direction
+            if (direction.name().charAt(0) == startingDirection) {
+                return direction;
+            }
+        }
+        throw new IllegalArgumentException("Cannot initialize starting direction to be " + startingDirection);
+    }
+
+    // method that uses modular arithmetic to calculate the direction the explorer faces after turning left
+    public Direction turnLeft() {
+        int directionValue = getDirectionValue();
+        Direction[] directionArray = getDirections();
+        return directionArray[(directionValue + 3) % 4];  // returns the new direction using the position in the Directions[] array
+    }  
+
+    // method that uses modular arithmetic to calculate the direction the explorer faces after turning left
+    public Direction turnRight() {
+        int directionValue = getDirectionValue();
+        Direction[] directionArray = getDirections();
+        return directionArray[(directionValue + 1) % 4];  // returns the new direction using the position in the Directions[] array
+    }  
+
+    public abstract void makeMove(int[] currentPosition, Maze maze);  // abstract method to move the explorer based on the current direction it faces
+}   
--- src/main/java/ca/mcmaster/se2aa4/mazerunner/DirectionAnalyzer.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/DirectionAnalyzer.java
@@ -1,96 +1,48 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 public class DirectionAnalyzer {
-    private char[] directions;
+    private Direction currentDirection;
     private int[] position;
-    private int currentDirection; 
     private Maze maze;
 
     public DirectionAnalyzer(char startingDirection, Maze maze, int[] entrance) {
         setInitialDirection(startingDirection);
-        this.directions = new char[] {'N', 'E', 'S', 'W'};  // corresponds the direction value to its array index to easily retrieve the direction
         this.position = entrance;  // assigns starting position to the entrance position
         this.maze = maze;
     }
 
     public void setInitialDirection(char direction) {
-        // uses a char to represent the direction that the explorer is facing when starting
-        switch(direction) {
-            case 'N':
-                this.currentDirection = 0;
-                break;
-            case 'E':
-                this.currentDirection = 1;
-                break;
-            case 'S':
-                this.currentDirection = 2;
-                break;
-            case 'W':
-                this.currentDirection = 3;
-                break;
-        }
+        this.currentDirection = Direction.NORTH;  // initializes the direction to be north
+        this.currentDirection = currentDirection.setInitialDirection(direction);  // updates starting direction to face the corrct way
     }
 
     public void moveExplorer(char move) {
-        // assigns current row and column to variables for better readability in 'F' case
-        int currentRow = this.position[0];
-        int currentColumn = this.position[1];
-        
-        // uses modular arithmetic to calculate the new position of the explorer
+        // uses the enum method to update the facing direction when turning left
         if (move =='L') {
-            this.currentDirection = (this.currentDirection + 3) % 4;
+            currentDirection = currentDirection.turnLeft();  
         }
+        // uses the enum method to update the facing direction when turning right
         else if (move == 'R') {
-            this.currentDirection = (this.currentDirection + 1) % 4;
+            currentDirection = currentDirection.turnRight();
         }
-        // moves the explorers position based on what direction it currently faces
+        // moves the explorers position by calling the enum method
         else if (move == 'F') {
-            switch(getFacingDirection()) {
-                case 'N':
-                    // moves the explorer up the maze when reading 'F' in North direction
-                    if (currentRow - 1 >= 0 && maze.getTile(currentRow - 1, currentColumn) == ' ') {
-                        this.position[0] = currentRow - 1;
-                    }
-                    
-                    break;
-                case 'E':
-                    // moves the explorer to the right when reading 'F' in East direction
-                    if (currentColumn + 1 < maze.getLength() && maze.getTile(currentRow, currentColumn + 1) == ' ') {
-                        position[1] = currentColumn + 1;
-                    }
-                    break;
-                case 'S':
-                    // moves the explorer down the maze when reading 'F' in South direction
-                    if (currentRow + 1 < maze.getHeight() && maze.getTile(currentRow +1, currentColumn) == ' ') {
-                        this.position[0] = currentRow + 1;
-                    }
-                    break;
-                case 'W':
-                    // moves the explorer to the left when reading 'F' in West direction
-                    if (currentColumn - 1 >= 0 && maze.getTile(currentRow, currentColumn - 1) == ' ') {
-                        position[1] = currentColumn - 1;
-                    }
-                    break;
-            }
+            currentDirection.makeMove(position, maze);
         }
     }
 
     // getter method to retrieve the direction as its char representation
     public char getFacingDirection() {   
-        return this.directions[this.currentDirection];
+        return currentDirection.name().charAt(0);
+    }
+
+    // getter method to retrieve the value of the facing direction
+    public int getFacingDirectionValue() {
+        return currentDirection.getDirectionValue();
     }
 
     // getter method to access the current position of the traveler
     public int[] getPosition() {
         return this.position;
-    }
-
-    public int getFacingDirectionValue() {
-        for (int i = 0; i < directions.length; i++) {
-            if (directions[i] == getFacingDirection()) {
-                return i;
-            }
-        }
-        throw new IllegalArgumentException("Invalid move direction.");
     }
 }
